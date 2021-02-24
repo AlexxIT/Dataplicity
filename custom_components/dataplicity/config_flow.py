a23bbe33385ee5ff688a8ee1b18b3ba3fc1b4261
Alexey Khit <alexey.khit@gmail.com>
2021-02-24 17:55:22 +0300

    Add errors about win32, SSL and wrong URL

--- custom_components/dataplicity/config_flow.py
+++ custom_components/dataplicity/config_flow.py
@@ -10,13 +10,16 @@ from . import DOMAIN, utils
 
 _LOGGER = logging.getLogger(__name__)
 
-RE_TOKEN = re.compile(r'https://www\.dataplicity\.com/([^.]+)\.py')
+RE_TOKEN = re.compile(r'https://www\.dataplicity\.com/([a-z0-9]+)\.py')
 
 
 class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, data=None, error=None):
         if sys.platform == 'win32':
-            return self.async_abort('win32')
+            return self.async_abort(reason='win32')
+
+        if self.hass.config.api.use_ssl:
+            return self.async_abort(reason='ssl')
 
         if data is None:
             return self.async_show_form(
@@ -29,6 +32,9 @@ class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
 
         m = RE_TOKEN.search(data['token'])
         token = m[1] if m else data['token']
+
+        if not token.isalnum():
+            return await self.async_step_user(error='token')
 
         session = async_get_clientsession(self.hass)
         resp = await utils.register_device(session, token)
